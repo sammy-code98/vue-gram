a453c81d2b2ab974f982ec2fad734073e42ec2bc
sammy-code98 <sammysmart98@yahoo.com>
2021-12-13 19:14:51 +0100

    feat:add posts and filters

--- src/components/PhoneSection.vue
+++ src/components/PhoneSection.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="phone-body">hdhdhehd</div>
+  <div class="phone-body">
+    <div class="feed">
+      <vuegram-post
+        v-for="post in posts"
+        :post="post"
+        :key="posts.indexOf(post)"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import vuegramPost from "./VuegramPost.vue";
 export default {
   name: "PhoneBody",
+  props: {
+    posts: Array,
+    filters: Array,
+  },
+  components: {
+    "vuegram-post": vuegramPost,
+  },
 };
 </script>
 
