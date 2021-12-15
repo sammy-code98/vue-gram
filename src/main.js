dd48433de5645fc7b6b2a783390f38a922f10a26
sammy-code98 <sammysmart98@yahoo.com>
2021-12-15 22:35:36 +0100

    feat:add  dragscroll to dependencie

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueDragscroll from "vue-dragscroll";
+
+Vue.use(VueDragscroll);
 
 Vue.config.productionTip = false;
 
